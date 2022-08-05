7f97db7c2cec817b5ee6eff7b62777608623fe5d
diliupg <unitygamecodersl@gmail.com>
2022-08-05 23:14:56 +0530

    L85 Draw Debug Line complete.

--- Source/CryptRobber/Grabber.cpp
+++ Source/CryptRobber/Grabber.cpp
@@ -2,9 +2,12 @@
 
 
 #include "Grabber.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values for this component's properties
-UGrabber::UGrabber()
+UGrabber::UGrabber() :
+	MaxGrabberDistance(500.f )
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -29,8 +32,10 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	FRotator MyRotation = GetComponentRotation( );
-	FString RotationStr = MyRotation.ToCompactString( );
-	UE_LOG( LogTemp, Warning, TEXT( "Grabber Rotation: %s" ), *RotationStr );
+	FVector Start = GetComponentLocation( );
+	FVector End = Start + GetForwardVector( ) * MaxGrabberDistance;
+
+	DrawDebugLine( GetWorld( ), Start, End, FColor::Red );
+
 }
 
